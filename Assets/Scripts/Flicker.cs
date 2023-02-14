a4f1e55b4a04dc8f9e0b9041611e2f2421aaeec8
Tyler <Weresquirrel@icloud.com>
2023-02-13 21:43:31 -0700

    test

--- Assets/Scripts/Flicker.cs
+++ Assets/Scripts/Flicker.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Flicker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float lightIntensity;
+    public float flickerIntensity;
+
+    public float lightTime;
+    public float flickerTime;
+
+    System.Random rg;
+
+    Light2D flashlight;
+
+    void Awake()
     {
-        
+        rg = new System.Random();
+        flashlight = GetComponent<Light2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        StartCoroutine(Flickerstuff());
+    }
+
+    IEnumerator Flickerstuff()
+    {
+        while (true)
+        {
+            flashlight.intensity = lightIntensity;
+
+            float lightingTime = lightTime + ((float)rg.NextDouble() - 0.5f);
+            yield return new WaitForSeconds(lightingTime);
+
+            int flickerCount = rg.Next(4, 9);
+
+            for (int i = 0; i < flickerCount; i++)
+            {
+                float flickingIntensity = lightIntensity - ((float)rg.NextDouble() * flickerIntensity);
+                flashlight.intensity = flickingIntensity;
+
+                float flickingTime = (float)rg.NextDouble() * flickerTime;
+                yield return new WaitForSeconds(flickingTime);
+            }
+        }
     }
 }
