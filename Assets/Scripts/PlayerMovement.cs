fbf64b603cef55799670c66acb9fd874df329ddd
Tyler <Weresquirrel@icloud.com>
2023-02-14 21:12:32 -0700

    Even Better Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -43,7 +43,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         input.Movement.Side.performed += input => move = input.ReadValue<float>();
         input.Movement.Jump.performed += input => jump = input.ReadValue<float>();
@@ -60,22 +60,25 @@ public class PlayerMovement : MonoBehaviour
     void HandleMovement()
     {
             isGrounded = (Physics2D.Raycast((new Vector2(this.transform.position.x, this.transform.position.y+1f)), Vector3.down, 2f, 1 << LayerMask.NameToLayer("Ground"))); // raycast down to look for ground is not detecting ground? only works if allowing jump when grounded = false; // return "Ground" layer as layer
-           
 
+        Debug.Log(rb.velocity.magnitude);
         if (isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jump * jumpSpeed);
 
         } 
-        if(isGrounded && rb.velocity.magnitude < maxWalkSpeed)
+        if(rb.velocity.magnitude < maxWalkSpeed)
         {
             rb.AddForce(new Vector2(move * acceleration, 0));
         }
-        else if (!isGrounded && rb.velocity.magnitude < maxWalkSpeed/1.5f)
+        if (rb.velocity.magnitude > maxWalkSpeed && rb.velocity.x > 0 && move < 0)
         {
-            rb.AddForce(new Vector2(move * acceleration / 1.5f, 0));
+            rb.AddForce(new Vector2(move * acceleration, 0));
         }
-
+        if (rb.velocity.magnitude > maxWalkSpeed && rb.velocity.x < 0 && move > 0)
+        {
+            rb.AddForce(new Vector2(move * acceleration, 0));
+        }
         if (rb.velocity.magnitude > maxSpeed)
         {
             rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxWalkSpeed);
